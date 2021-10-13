c5555a5029549c2f4044b1078f60bed53179b51b
Ben Altair <ben@benaltair.com>
2021-10-13 16:58:37 -0700

    Add template

--- src/layouts/default.svelte
+++ src/layouts/default.svelte
@@ -9,14 +9,23 @@
   <title>{title}</title>
 </svelte:head>
 
-<!-- <nav>
-  <ul>
-    {#each routes as route}
-      <li>
-        <a href={route.path}>{route.label}</a>
-      </li>
-    {/each}
-  </ul>
-</nav> -->
+<header>
+  <h1>{title}</h1>
+  <!-- <nav>
+    <ul>
+      {#each routes as route}
+        <li>
+          <a href={route.path}>{route.label}</a>
+        </li>
+      {/each}
+    </ul>
+  </nav> -->
+</header>
 
-<slot />
+<main>
+  <slot />
+</main>
+
+<footer>
+  <!-- <p></p> -->
+</footer>
