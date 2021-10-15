508db70d3f40767eb4cb1d686f3f82666a879d04
Ben Altair <ben@benaltair.com>
2021-10-14 23:43:34 -0700

    Add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Default from "/src/layouts/default.svelte";
+  export let status, error;
+  let title = `${status}: ${error.message}`;
+</script>
+
+<Default {title}>
+  <h1>{title}</h1>
+  <p>{error.message}</p>
+</Default>
